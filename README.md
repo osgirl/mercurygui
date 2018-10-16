2b110944eae3d6928da2edcea059275507ec6fa0
Sam Schott <ss2151@cam.ac.uk>
2018-10-16 12:45:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mercurygui
+mercurygui provides a higher-level worker thread which regularly queries the MercuryiTC for its sensor readings and provides a live stream of this data to other parts of the software. This prevents individual functions from querying the MercuryiTC directly and causing unnecessary overhead.
+
+The user interface for the cryostat plots historic temperature readings going back up to 24 h and provides access to relevant temperature control settings such as gas flow, heater power, and ramping speed while lower-level configurations such as calibration tables must be changed programatically.
+
+## System requirements
+*Required*:
+
+- Linux or macOS
+- Python 2.7 or 3.x
+
+## Acknowledgements
+Config file modules are based on the implementation from [Spyder](https://github.com/spyder-ide).
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mercurygui',
+    version='0.0.1',
+    description="",
+    author='Sam Schott',
+    author_email='ss2151@cam.ac.uk',
+    url='https://github.com/oe-fet/mercurygui.git',
+    license='MIT',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    package_data={
+        'mercurygui': ['*.ui']
+    },
+    entry_points={
+        'console_scripts': [
+            'mercurygui=mercurygui.main:main'
+        ],
+        'gui_scripts': [
+            'mercurygui=mercurygui.main:main'
+        ]
+    },
+    install_requires=[
+        'setuptools',
+        'QtPy',
+        'mercuryitc',
+        'matplotlib',
+        'pyvisa',
+        'setuptools',
+        'QDarkStyle',
+        'repr',
+        'spyder'
+    ],
+    zip_safe=False,
+    keywords='mercurygui',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=[
+    ]
+)
