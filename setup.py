f479ee9710dc89be42bdc1d046b9ea4dd48a0ecf
Sam Schott <ss2151@cam.ac.uk>
2018-12-10 14:38:28 +0000

    fixed oeverlapping figure canvas and label

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mercurygui',
-    version='0.0.1',
+    version='0.1.0',
     description="",
     author='Sam Schott',
     author_email='ss2151@cam.ac.uk',
